e9c71859ae3813ec021e852988a18b14a1740c4d
Hamlet <danielhamlet123@gmail.com>
2021-10-19 17:45:08 -0400

    Primer Commit

--- Aplicación de Caja/Aplicación_Caja/Aplicación_Caja/Form1.Designer.cs
+++ Aplicación de Caja/Aplicación_Caja/Aplicación_Caja/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Aplicación_Caja
 {
-    partial class Form1
+    partial class Inicio
     {
         /// <summary>
         /// Required designer variable.
@@ -29,10 +29,18 @@ namespace Aplicación_Caja
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Name = "Inicio";
+            this.Text = "Aplicación de Caja";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- Aplicación de Caja/Aplicación_Caja/Aplicación_Caja/Form1.cs
+++ Aplicación de Caja/Aplicación_Caja/Aplicación_Caja/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Aplicación_Caja
 {
-    public partial class Form1 : Form
+    public partial class Inicio : Form
     {
-        public Form1()
+        public Inicio()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
